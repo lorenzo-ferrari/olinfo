bc01558ba7d49ac46199a27bb891b6a8416c3031
lorenzo-ferrari <lorenzo.ferrari@lsgalilei.org>
2021-10-24 22:28:10 +0200

    last upd 2021_10_24

---
+++ poi.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+using namespace std;
+
+int main() {
+#ifdef LORENZO
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#endif
+
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int n, t, p;
+  cin >> n >> t >> p;
+  vector<int> pt(t, n);
+  vector<int> cnt(n, 0);
+  vector<vector<bool>> solved(n, vector<bool>(t));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < t; ++j) {
+      int t;
+      cin >> t;
+      pt[j] -= (t == 1);
+      solved[i][j] = t;
+      cnt[i] += t;
+    }
+  }
+  vector<int> scores(n, 0);
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < t; ++j)
+      if (solved[i][j])
+        scores[i] += pt[j];
+  int pos = 1;
+  for (int i = 0, j = p - 1; i < n; ++i) {
+    if (scores[i] > scores[j] ||
+        (scores[i] == scores[j] &&
+         (cnt[i] > cnt[j] || (cnt[i] == cnt[j] && i < j))))
+      ++pos;
+  }
+  cout << scores[p-1] << " " << pos << "\n";
+}
