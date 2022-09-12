c70fbad321aa43bc67e1e6a6c642221cd8e5ed1d
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2022-09-12 09:03:22 +0200

    .

---
+++ src/versi.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+
+int direziona(int n, vector<int> a, vector<int> b) {
+    int A = 0;
+    for (int i = 0, j = 0; i < n; ++i) {
+        while (j != n && b[j] < a[i]) ++j;
+        if (j != n) {
+            ++A;
+            ++j;
+        }
+    }
+    int B = 0;
+    for (int i = 0, l = 0; i < n; ++i) {
+        if (b[l] < a[i]) {
+            ++l;
+        } else {
+            ++B;
+        }
+    }
+    return A - B + 1;
+}
