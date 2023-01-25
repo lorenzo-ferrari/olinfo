a5fea5af897befa26e8b4c43c44705f30886da6c
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2023-01-25 12:55:46 +0100

    .

--- src/crocodile.cpp
+++ src/crocodile.cpp
@@ -1,61 +1,38 @@
 #include <bits/stdc++.h>
-#pragma GCC optimize ("O3")
-#pragma GCC optimize ("Ofast")
 using namespace std;
+using LL = long long;
 
-const int64_t INF = 1e12;
-
-vector <int64_t> d1;
-vector <int64_t> d2;
-vector <vector<pair<int64_t, int64_t>>> g;
-
-int travel_plan(int n, int m, int r[][2], int l[], int k, int p[]) {
-	g.resize(n);
-	d1.assign(n, INF);
-	d2.assign(n, INF);
-
-	for (int i = 0; i < m; i++) {
-		g[r[i][0]].push_back({r[i][1], l[i]});
-		g[r[i][1]].push_back({r[i][0], l[i]});
-	}
-
-	priority_queue <pair<int64_t, int64_t>> Q;
-	for (int i = 0; i < k; i++) {
-		d1[p[i]] = 0;
-		d2[p[i]] = 0;
-		Q.push({0, p[i]});
-	}
-
-	vector <bool> vis(n);
-	while (!Q.empty()) {
-		int64_t v = Q.top().second;
-		Q.pop();
-
-		if (vis[v])
-			continue;
-		vis[v] = true;
-
-		for (auto e : g[v]) {
-			int64_t u = e.first;
-			int64_t w = e.second;
-			if (d1[u] > d2[v] + w) {
-				d2[u] = d1[u];
-				d1[u] = d2[v] + w;
-
-				if (d2[u] != INF) {
-					Q.push({-d2[u], u});
-				}
-			} else if (d2[u] > d2[v] + w) {
-				d2[u] = d2[v] + w;
-
-				Q.push({-d2[u], u});
-			}
-		}
-	}
-
-	// for (int i = 0; i < n; i++) {
-	// 	cout << d1[i] << " " << d2[i] << "\n";
-	// }
-
-	return d2[0];
+LL travel_plan(int n, int m, int r[][2], int l[], int k, int p[]) {
+    vector<vector<array<LL, 2>>> adj(n);
+    for (int i = 0; i < m; ++i) {
+        adj[r[i][0]].push_back({r[i][1], l[i]});
+        adj[r[i][1]].push_back({r[i][0], l[i]});
+    }
+    constexpr LL INF = 1e15;
+    vector<array<LL, 2>> d(n, {INF, INF});
+    priority_queue<array<LL, 2>> Q;
+    for (int i = 0; i < k; ++i) {
+        d[p[i]][0] = d[p[i]][1] = 0;
+        Q.push({0, p[i]});
+    }
+    vector<bool> vis(n);
+    while (!Q.empty()) {
+        int v = Q.top()[1];
+        Q.pop();
+        if (vis[v]) {
+            continue;
+        }
+        vis[v] = true;
+        for (auto [u, w] : adj[v]) {
+            if (d[u][0] >= d[v][1] + w) {
+                d[u][1] = d[u][0];
+                d[u][0] = d[v][1] + w;
+                Q.push({-d[u][1], u});
+            } else if (d[u][1] > d[v][1] + w) {
+                d[u][1] = d[v][1] + w;
+                Q.push({-d[u][1], u});
+            }
+        }
+    }
+    return d[0][1];
 }
--- src/ois_cannons.cpp
+++ src/ois_cannons.cpp
@@ -1,68 +1,30 @@
-#include <queue>
-#include <vector>
-#include <iostream>
-#pragma GCC optimize ("O3")
+#include <bits/stdc++.h>
 using namespace std;
-#define F first
-#define S second
-
-inline int get_int() {
-    int n = 0;
-    char c = getchar_unlocked();
-    while (c >= '0') {
-        n = (n << 3) + (n << 1) + c - '0';
-        c = getchar_unlocked();
-    }
-    return n;
-}
-
-inline void print(int n) {
-    int i = 0;
-    char S[20];
-    if (n == 0) {
-        putchar_unlocked('0');
-        putchar_unlocked(' ');
-        return;
-    }
-    while (n) {
-        S[i++] = n % 10 + '0';
-        n /= 10;
-    }
-    while (--i >= 0)
-        putchar_unlocked(S[i]);
-    putchar_unlocked(' ');
-}
-
-const int INF = 1e9;
-const int MAXN = 1e6;
 
 int main() {
-	int n = get_int();
-	int t[MAXN];
-	for (int i = 0; i < n; i++)
-		t[i] = get_int();
-
-	int d[MAXN];
-	fill(d, d + n, INF);
-	priority_queue <pair<int, int>> Q;
-	d[0] = 0;
-	Q.push({0, 0});
-	while (!Q.empty()) {
-		int v = Q.top().S;
-		Q.pop();
-		if (d[t[v]] > d[v]) {
-			d[t[v]] = d[v];
-			Q.push({-d[t[v]], t[v]});
-		}
-		if (v != 0 && d[v - 1] > d[v] + 1) {
-			d[v - 1] = d[v] + 1;
-			Q.push({-d[v - 1], v - 1});
-		}
-		if (v != n - 1 && d[v + 1] > d[v] + 1) {
-			d[v + 1] = d[v] + 1;
-			Q.push({-d[v + 1], v + 1});
-		}
-	}
-
-	print(d[n - 1]);
+    int n; cin >> n;
+    vector<int> t(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> t[i];
+    }
+    vector<int> d(n, 1e9);
+    priority_queue<array<int, 2>> Q;
+    d[0] = 0;
+    Q.push({0, 0});
+    while (!Q.empty()) {
+        int v = Q.top()[1];
+        Q.pop();
+        if (d[t[v]] > d[v]) {
+            d[t[v]] = d[v];
+            Q.push({-d[v], t[v]});
+        }
+        for (int dx = -1; dx <= 1; dx += 2) {
+            int u = v + dx;
+            if (0 <= u && u < n && d[u] > d[v] + 1) {
+                d[u] = d[v] + 1;
+                Q.push({-d[u], u});
+            }
+        }
+    }
+    cout << d[n-1] << "\n";
 }
