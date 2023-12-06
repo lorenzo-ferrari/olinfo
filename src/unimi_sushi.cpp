e59bfd8d476e974bea89b6fbf5573385b54ce29a
Lorenzo Ferrari <potassiopermanganato@gmail.com>
2023-12-06 18:55:49 +0100

    .

--- src/unimi_sushi.cpp
+++ src/unimi_sushi.cpp
@@ -4,9 +4,6 @@ using namespace std;
 #define all(x) begin(x),end(x)
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     int n; cin >> n;
     vector<array<int, 3>> items(n);
     vector<int> z;
